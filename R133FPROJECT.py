b0d722c773ffd7c41de73ea346c150ef0b3d2e67
R133FGANZ <50979010+SCTcyber@users.noreply.github.com>
2019-09-07 15:20:17 +0700

    Add files via upload

---
+++ R133FPROJECT.py
@@ -0,0 +1,99 @@
+import socket
+import os
+import random
+import sys
+import time
+from time import sleep
+os.system('clear')
+def mengetik(s):
+    for c in s  + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() + 0.4)
+print ('\033[1;32mLoading...')
+sleep(0.1)
+mengetik('( >>>>>>>>>>>>>> )')
+os.system('clear')
+sleep(1)
+def main():
+
+         print ('\033[1;32m   _______________________')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║')
+         sleep(0.2)
+         print ('  ║▒▒▒▒▒▒▒▒▒▒║       ╔════════════╗ ')
+         sleep(0.2)
+         print ('  ║██████████╚╗       Author  : Mr.4R13F')
+         sleep(0.2)
+         print ('  ║██╔══╗█╔═╗█║       Team    : DevlinCyber')
+         sleep(0.2)
+         print ('  ║██║╬╔╝█╚╗║█║       Jr Team : AloneCyberSecurity')
+         sleep(0.2)
+         print ('  ║██╚═╝█║█╚╝█║      ╚════════════╝')
+         sleep(0.2)
+         print ('  ╚╗█████████═╝')
+         sleep(0.2)
+         print ('   ╚╗║╠╩╩╩╩╩╝')
+         sleep(0.2)
+         print ('    ║║┈┈┈█▐█████▒.｡oO')
+         sleep(0.2)
+         print ('    ║██╠╦╦╦╗')
+         sleep(0.2)
+         print ('    ╚╗██████')
+         sleep(0.2)
+         print ('     ╚════╝')
+main()
+def main():
+         print ('\033[1;31m1.Daface ?')
+         print ('\033[1;31m2.Ddos ?')
+         print ('\033[1;31m3.Keluar ?')
+
+         nomor = input ('Pilih nomor  :')
+         if nomor == '1':
+              sleep(2)
+              print ('\033[1;32mTunggu sebentar')
+              mengetik('\033[1;33mwait...')
+              os.system('clear')
+              print ('\033[1;32mLoading...')
+              mengetik('( >>>>>>>>>>>>>> )')
+              sleep(2)
+              os.system('clear')
+              print ('       ╔╗  ╔╗         ')
+              print ('╔╦╦╗╔═╗║╚╗╔╝║╔═╗ ╔═╦═╗')
+              print ('║║║║║╩╣║╬║║╬║║╬╚╗╚╗║╔╝')
+              print ('╚══╝╚═╝╚═╝╚═╝╚══╝ ╚═╝ ')
+              print ('###########################################################')
+              print ("# command : curl -T /storage/emulated/0/[script] [target] #")
+              print ("#                                                         #")
+              print ("# Mr.4R13F                                    DevlinCyber #")
+              print ("###########################################################")
+
+         else:
+              print ('\033[1;37mMakasih Bro telah gunain tools ini')
+
+         if nomor == '2' :
+              sleep(2)
+              print ('\033[1;32mtunggu sebentar')
+              mengetik('\033[1;33mwait...')
+              os.system('clear')
+              print ('\033[1;33mLoading...')
+              mengetik('( >>>>>>>>>>>>>>> )')
+              os.system('clear')
+              os.system('toilet -f slant --gay DDOS')
+              sleep(3)
+              os.system('python2 R133FDDOS.py')
+              print ('\033[1;31mSelamat Mencoba')
+
+         else:
+              print ('\033[1;37mMakasih Bro Telah gunain tools ini')
+
+main()
+
